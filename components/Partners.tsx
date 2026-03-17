8004c87b3f93c5a1cbe97e43cb3459701af37756
Gperyy <gulperiozblt@gmail.com>
2026-03-17 12:40:35 +0300

    .

--- components/Partners.tsx
+++ components/Partners.tsx
@@ -76,7 +76,14 @@ const Partners: React.FC<PartnersProps> = ({ lang, onNavigate }) => {
   ].sort((a, b) => {
   const lastNameA = a.split(' ').pop() || '';
   const lastNameB = b.split(' ').pop() || '';
-  return lastNameA.localeCompare(lastNameB, 'tr');
+  const normalizeTurkish = (str: string) => str
+    .replace(/Ç/g, 'C').replace(/ç/g, 'c')
+    .replace(/Ğ/g, 'G').replace(/ğ/g, 'g')
+    .replace(/İ/g, 'I').replace(/ı/g, 'i')
+    .replace(/Ö/g, 'O').replace(/ö/g, 'o')
+    .replace(/Ş/g, 'S').replace(/ş/g, 's')
+    .replace(/Ü/g, 'U').replace(/ü/g, 'u');
+  return normalizeTurkish(lastNameA).localeCompare(normalizeTurkish(lastNameB));
 });
 
   return (
